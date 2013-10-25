eaaec3b5452d86d432f18e6a418e6462016b2e7a
Andreas Koch <koch@marmalade.de>
2013-10-25 15:29:09 +0200

    changes for oxid 4.6

--- core/ocb_cleartmp_oxshopcontrol.php
+++ core/ocb_cleartmp_oxshopcontrol.php
@@ -10,7 +10,7 @@ class ocb_cleartmp_oxshopcontrol extends ocb_cleartmp_oxshopcontrol_parent
     
     protected function _runOnce()
     {
-        $oConf     = oxRegistry::getConfig();
+        $oConf     = $this->getConfig();
         $blDevMode = $oConf->getShopConfVar('blDevMode',null,'module:ocb_cleartmp');
         
         
@@ -19,8 +19,7 @@ class ocb_cleartmp_oxshopcontrol extends ocb_cleartmp_oxshopcontrol_parent
             
             $sTmpDir = realpath($oConf->getShopConfVar('sCompileDir'));
             $aFiles = glob($sTmpDir.'/*{.php,.txt}',GLOB_BRACE);
-            $aFiles = array_merge($aFiles, glob($sTmpDir.'/smarty/*.php'));
-            $aFiles = array_merge($aFiles, glob($sTmpDir.'/ocb_cache/*.json'));
+            $aFiles = array_merge($aFiles, glob($sTmpDir.'/*.tpl.php'));
             if(count($aFiles) > 0)
             {
                 foreach($aFiles as $file) {
